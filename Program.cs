92f1bcd09286a1ba0a83277f9c14177b3823e842
freeman <freeman3x@mail.ru>
2023-01-04 18:33:43 +0300

    Создал массив

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строr,
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+//Примеры:
+//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+//[“Russia”, “Denmark”, “Kazan”] → []
+
+string [] CreatedNewArray()
+{
+    string[] array = new string[5];
+    array[0] = "Hello";
+    array[1] = "2";
+    array[2] = "world";
+    array[3] = ":-";
+    array[4] = "Russia";
+ return array;
+}
+
+
